6499163c17265cfcede54d5c52df9e4d93ffd5fb
shaoyanpan <89927506+shaoyanpan@users.noreply.github.com>
2021-10-17 18:43:36 -0400

    Fix the function name

--- python/deid_date_year_ShaoyanPan.py
+++ python/deid_date_year_ShaoyanPan.py
@@ -122,6 +122,5 @@ def deid_dateyear(text_path= 'id.text', output_path = 'phone.phi'):
                     chunk = ''
                 
 if __name__== "__main__":
-    
-    deid_phone(sys.argv[1], sys.argv[2])
+    deid_dateyear(sys.argv[1], sys.argv[2])
     
